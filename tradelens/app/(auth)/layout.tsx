158f5acf25445e19b10ce05b8f281ad0854ed44d
Nitin Patel <nitpatel678@gmail.com>
2025-10-05 18:07:43 +0530

    Implemented auth ui and layout

---
+++ tradelens/app/(auth)/layout.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import Image from "next/image";
+// import {auth} from "@/lib/better-auth/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { Star } from "lucide-react";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  // const session = await auth.api.getSession({ headers: await headers() })
+
+  // if(session?.user) redirect('/')
+
+  return (
+    <main className="auth-layout">
+      <section className="auth-left-section scrollbar-hide-default">
+        <Link href="/" className="auth-logo">
+          <Image
+            src="/logo.png"
+            alt="TradeLens logo"
+            width={140}
+            height={32}
+            className="h-10 w-auto"
+          />
+        </Link>
+
+        <div className="pb-6 lg:pb-8 flex-1">{children}</div>
+      </section>
+
+      <section className="auth-right-section">
+        <div className="z-10 relative lg:mt-4 lg:mb-16">
+          <blockquote className="auth-blockquote">
+            TradeLens turned my watchlist into a winning list. The alerts are
+            spot-on, and I feel more confident making moves in the market
+          </blockquote>
+          <div className="flex items-center justify-between">
+            <div>
+              <cite className="auth-testimonial-author">- Ethan R.</cite>
+              <p className="max-md:text-xs text-gray-500">Retail Investor</p>
+            </div>
+            <div className="flex items-center gap-0.5">
+              {[1, 2, 3, 4, 5].map((star) => (
+                <Star
+                  key={star}
+                  size={20}
+                  className="text-orange-400"
+                  fill="currentColor"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1 relative">
+          <Image
+            src="/dashboard.png"
+            alt="Dashboard Preview"
+            width={1440}
+            height={1150}
+            className="auth-dashboard-preview absolute top-0"
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
+export default Layout;
