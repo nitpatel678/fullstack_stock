bc662a50044efeba13418dedcff3ac2a9070d8ac
Nitin Patel <nitpatel678@gmail.com>
2025-10-05 22:12:08 +0530

    update

--- tradelens/types/global.d.ts
+++ tradelens/types/global.d.ts
@@ -218,3 +218,18 @@ declare global {
 }
 
 export {};
+
+
+declare module 'react-select-country-list' {
+  interface Country {
+    label: string;
+    value: string;
+  }
+
+  interface CountryList {
+    getData: () => Country[];
+  }
+
+  const countryList: () => CountryList;
+  export default countryList;
+}
